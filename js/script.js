3a71978ac54d9d5bdacda6035717878945394ccd
sergiy-kyiv-2022 <s22101969@gmail.com>
2023-08-06 12:33:06 +0200

    vb

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-function painting(x,y){
+/*function painting(x,y){
     const sum = x+y;
+    const dot =x*y;
     console.log(sum);
+    alert
 };
-painting(10,25);
+painting(10,25);*/
+
+function showMessage(from, text) { // параметри: from, text
+    alert(from + ': ' + text);
+  }
+  
+  showMessage('Анна', 'Привіт!'); // Анна: Привіт! (*)
+  showMessage('Анна', "Як справи?"); // Анна: Як справи? (**)
